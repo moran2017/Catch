a53c4e83773453d8458d8c937c8ac8a262ef8c37
moran2017 <31378556+moran2017@users.noreply.github.com>
2017-09-20 18:46:42 -0500

    creacion de package
    adapters
    fragment
    module

--- CatchUp2/app/src/main/java/pe/edu/upc/catchup/activites/MainActivity.java
+++ CatchUp2/app/src/main/java/pe/edu/upc/catchup/activites/MainActivity.java
@@ -1,4 +1,4 @@
-package pe.edu.upc.catchup;
+package pe.edu.upc.catchup.activites;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import pe.edu.upc.catchup.R;
 
 public class MainActivity extends AppCompatActivity {
 
